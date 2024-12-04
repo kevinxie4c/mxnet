ca404b47b3e0e28321ef61c651b8f552f7fb04f6
Kaixiang Xie <kaixiang.xie@mail.mcgill.ca>
2024-12-04 14:57:28 -0500

    script for building

---
+++ build-gpu.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+mkdir build-gpu
+cd build-gpu
+#sudo apt install ccache libopenblas-dev libopencv-dev
+cmake -DUSE_CUDA=ON -DUSE_MKL_IF_AVAILABLE=OFF -DCMAKE_CUDA_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_INSTALL_PREFIX=../ubuntu-22.04-cpu -DCMAKE_BUILD_TYPE=Distribution ..
+# CMAKE_BUILD_TYPE=Distribution to set the corrent rpath
