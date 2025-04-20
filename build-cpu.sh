b49603224622e6578e4779a4755a080129e75742
Kaixiang Xie <kaixiang.xie@mail.mcgill.ca>
2025-04-20 00:32:13 -0400

    modify build options

--- build-cpu.sh
+++ build-cpu.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+CWD=$PWD
 mkdir build-cpu
 cd build-cpu
 #sudo apt install ccache libopenblas-dev libopencv-dev
-cmake -DUSE_CUDA=OFF -DUSE_MKL_IF_AVAILABLE=OFF -DCMAKE_CUDA_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_INSTALL_PREFIX=../ubuntu-22.04-cpu -DCMAKE_BUILD_TYPE=Distribution ..
+cmake -DUSE_CUDA=OFF -DUSE_MKL_IF_AVAILABLE=OFF -DCMAKE_CUDA_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_INSTALL_PREFIX=$CWD/ubuntu-22.04-cpu -DCMAKE_BUILD_TYPE=Distribution ..
 # CMAKE_BUILD_TYPE=Distribution to set the corrent rpath
--- build-gpu.sh
+++ build-gpu.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+CWD=$PWD
 mkdir build-gpu
 cd build-gpu
 #sudo apt install ccache libopenblas-dev libopencv-dev
-cmake -DUSE_CUDA=ON -DUSE_MKL_IF_AVAILABLE=OFF -DCMAKE_CUDA_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_INSTALL_PREFIX=../ubuntu-22.04-cpu -DCMAKE_BUILD_TYPE=Distribution ..
+cmake -DCMAKE_CUDA_COMPILER=/usr/local/cuda-11.7/bin/nvcc -DUSE_CUDA=ON -DUSE_MKL_IF_AVAILABLE=OFF -DCMAKE_CUDA_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_INSTALL_PREFIX=$CWD/ubuntu-22.04-gpu -DCMAKE_BUILD_TYPE=Distribution ..
 # CMAKE_BUILD_TYPE=Distribution to set the corrent rpath
